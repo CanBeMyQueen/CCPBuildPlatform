745889ee288e64986c7050af73fa73737f90c02d
CanBeMyQueen <jiayin_user@163.com>
2016-11-10 10:56:52 +0800

    Initial Commit

---
+++ CCPBuildPlatform/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CCPBuildPlatform
+//
+//  Created by 尹佳 on 2016/11/10.
+//  Copyright © 2016年 尹佳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CCPBuildPlatform/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CCPBuildPlatform
+//
+//  Created by 尹佳 on 2016/11/10.
+//  Copyright © 2016年 尹佳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
